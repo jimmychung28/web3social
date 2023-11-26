171a4fcc8b1e32a18e9dd9fb5682d74bee127671
Jimmy Chung <jimmy46@hotmail.ca>
2023-11-25 21:19:29 -0500

    title

--- app/views/AppAppBar.tsx
+++ app/views/AppAppBar.tsx
@@ -23,7 +23,7 @@ function AppAppBar() {
             href="/premium-themes/onepirate/"
             sx={{ fontSize: 24 }}
           >
-            {'onepirate'}
+            {'Alpacpac 🦙'}
           </Link>
           <Box sx={{ flex: 1, display: 'flex', justifyContent: 'flex-end' }}>
             <Link
