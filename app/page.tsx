9070e2a1bcdf073ff8f0c4a2cfee97e37914d5da
Jimmy Chung <jimmy46@hotmail.ca>
2023-11-25 21:21:25 -0500

    format

--- app/page.tsx
+++ app/page.tsx
@@ -34,7 +34,7 @@ export default function Home() {
   return (
     <div>
         <ThemeProvider theme={theme}>
-      <AppAppBar />
+          <AppAppBar />
       </ThemeProvider>
     </div>
   )
